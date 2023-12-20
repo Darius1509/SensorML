bbbeae3c92d5120e006da6c5e4eb473740daff6b
Darius Dinu <dariusdinu19@gmail.com>
2023-12-20 23:33:39 +0200

    Updated README.md + Corelation Matrix + Added small Dataset

---
+++ CorelationMatrix.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def analyze_correlation(data, threshold=0.7):
+    #correlation matrix
+    correlation_matrix = data.corr()
+
+    #lists for corellated values
+    positive_correlation = []
+    negative_correlation = []
+
+    for col1 in correlation_matrix.columns:
+        for col2 in correlation_matrix.columns:
+            if col1 != col2 and col1 < col2:
+                correlation_value = correlation_matrix.loc[col1, col2]
+                if correlation_value > threshold:
+                    positive_correlation.append((col1, col2, correlation_value))
+                elif correlation_value < -threshold:
+                    negative_correlation.append((col1, col2, correlation_value))
+
+    #Correlation matrix as a heatmap
+    sns.heatmap(correlation_matrix, annot=True)
+    plt.show()
+
+    print("Perechi de variabile cu corelație pozitiva:")
+    for pair in positive_correlation:
+        print(f"{pair[0]} & {pair[1]}: {pair[2]}")
+    print("\nPerechi de variabile cu corelație negativa:")
+    for pair in negative_correlation:
+        print(f"{pair[0]} & {pair[1]}: {pair[2]}")
+
+
+dataset = pd.read_csv("SensorMLDataset_small.csv")
+selected_columns = ['pres', 'temp1', 'umid', 'temp2', 'lumina', 'temps1', 'temps2', 'V450', 'B500', 'G550', 'Y570', 'O600', 'R650']
+selected_data = dataset[selected_columns]
+
+analyze_correlation(selected_data)
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # SensorML
+
+### Link: [link to project desc](https://docs.google.com/document/d/16F6lZPURgEj36HYWYuvsnu5IIDpZDNnRAMI9-kE2Pjw/edit)
+
+### Team Members:
+-Birladeanu Rares\
+-Dinu Darius\
+-Pruteanu Matei\
+-Iosub Miruna\
+
+### Progress: WIP
+
+### Description:
+Datasetul SensorML conține date referitoare la evoluția în timp a unor parametri de mediu (temperatură, luminozitate, umiditate, presiune, etc.) pentru o cultură de tomate. Pornind de la acest dataset, realizați o aplicație care să: 
+Prezică evoluția în timp a parametrilor de mediu prin antrenarea mai multor modele specifice seriilor de timp. Se vor utiliza (minim):
+1 model statistic (Prophet, (S)ARIMA, etc.)
+1 model bazat pe o rețea neuronală recurentă (LSTM, GRU, etc.)
+1 model de tip Seq2Seq 
+Pentru implementarea modelelor puteți utiliza librării dedicate (de exemplu: PyTorch, TensorFlow,  tsai, statsmodels, etc.).
+
+Ofere o interfață web care să cuprindă graficele corespunzătoare predicțiilor realizate și informații referitoare la boli posibile ale plantelor, simptomele asociate acestora și condițiile optime de dezvoltare a bolilor. În plus, folosind predicțiile pentru valorile parametrilor și condițiile optime de dezvoltare a bolilor, aplicația va prezice riscul de boală. 
+
+
+### Tasks: 
+The task distribution can be checked in the <b>Issues</b> tab
