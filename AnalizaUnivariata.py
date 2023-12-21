a3ed2750bd02ccecf46626491b1ee7508be5cf76
Bîrlădeanu Rareș <birladeanurares@gmail.com>
2023-12-21 15:57:13 +0200

    uncommented

--- AnalizaUnivariata.py
+++ AnalizaUnivariata.py
@@ -11,43 +11,41 @@ df['Timestamp'] = df['Timestamp'].apply(lambda x: x.split()[0])
 
 df['Timestamp'] = pd.to_datetime(df['Timestamp'])
 
-# pdf_filename = 'MeanPlots.pdf'
-# with PdfPages(pdf_filename) as pdf:
-#     for column in columns[1:]:
-#         df2 = df[['Timestamp', column]]
-#
-#         df2 = df2.groupby('Timestamp').mean()
-#
-#         plt.figure(figsize=(20, 10))
-#         sns.heatmap(df2, cmap='coolwarm')
-#         plt.title(f"mean {column}")
-#
-#         pdf.savefig()
-#         plt.close()
-#
-# pdf_filename = 'MedianPlots.pdf'
-# with PdfPages(pdf_filename) as pdf:
-#     for column in columns[1:]:
-#         df2 = df[['Timestamp', column]]
-#
-#         df2 = df2.groupby('Timestamp').median()
-#
-#         plt.figure(figsize=(20, 10))
-#         sns.heatmap(df2, cmap='coolwarm')
-#         plt.title(f"median {column}")
-#
-#         pdf.savefig()
-#         plt.close()
+pdf_filename = 'MeanPlots.pdf'
+with PdfPages(pdf_filename) as pdf:
+    for column in columns[1:]:
+        df2 = df[['Timestamp', column]]
+
+        df2 = df2.groupby('Timestamp').mean()
+
+        plt.figure(figsize=(20, 10))
+        sns.heatmap(df2, cmap='coolwarm')
+        plt.title(f"mean {column}")
+
+        pdf.savefig()
+        plt.close()
+
+pdf_filename = 'MedianPlots.pdf'
+with PdfPages(pdf_filename) as pdf:
+    for column in columns[1:]:
+        df2 = df[['Timestamp', column]]
+
+        df2 = df2.groupby('Timestamp').median()
+
+        plt.figure(figsize=(20, 10))
+        sns.heatmap(df2, cmap='coolwarm')
+        plt.title(f"median {column}")
+
+        pdf.savefig()
+        plt.close()
 
 
-# use boxplot to find outliers
 pdf_filename = 'BoxPlots.pdf'
 with PdfPages(pdf_filename) as pdf:
 
     for column in columns[1:]:
         df2 = df[['Timestamp', column]]
 
-        # make one boxplot for all days combined
         plt.figure(figsize=(20, 10))
         sns.boxplot(x=df2[column])
         plt.title(f"boxplot {column}")
